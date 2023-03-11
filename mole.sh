7e09c470e9635d56b5d84794cbf90c379dde6167
Bálek Miroslav <mirbalek@seznam.cz>
2023-03-11 19:45:32 +0100

    idk

--- mole.sh
+++ mole.sh
@@ -1,5 +1,14 @@
 #! /bin/bash
 
+gflag=0
+aflag=0
+mflag=0
+bflag=0
+aval=0000-00-00
+bval=9999-99-99
+gval='.'
+dir=$(pwd)
+
 ArgError() {
     if [ "$gflag" -gt 1 ]; then
         err=1
@@ -23,6 +32,13 @@ ArgError() {
     fi
 }
 
+CreateMoleRC() {
+    #create mole_rc
+    if [[ ! -e $MOLE_RC ]]; then
+        touch $MOLE_RC
+    fi
+}
+
 PrintHelp() {
     echo "Usage:"
     exit 0
@@ -30,44 +46,64 @@ PrintHelp() {
 
 SetEditor() {
     if [ -n "$EDITOR" ]; then
-        edit_path="$EDITOR"
+        editor_path="$EDITOR"
 
     elif [ -n "$VISUAL" ]; then
-        edit_path="$VISUAL"
+        editor_path="$VISUAL"
     else
 
-        edit_path=$(which vi)
+        editor_path=$(which vi)
     fi
 }
 
-IsDirectory() {
+IsFile() {
     if [ -d "$path" ]; then
-        is_dir=1
         echo "DEBUG: Directory is $path"
     elif [ -f "$path" ]; then
         is_file=1
         echo "DEBUG: File is $path"
     else
-        echo "DEBUG: File or Directory $path does not exist."
-        exit 1
+        lastchar=${path: -1}
+        if [ "$lastchar" == "/" ]; then
+            echo "DEBUG: NonExisting Directory is $path"
+            exit 1
+        elif [ -n "$lastchar" ]; then
+            is_file=1
+            echo "DEBUG: NonExisting File is $path"
+        fi
     fi
 }
 
-File(){
+Filters() {
+    if [ -n "$gflag" ]; then
+        #ProcessGroups
+        echo
+    fi
+
+}
+File() {
+    date=$(date +'%Y-%m-%d')
+    timestamp=$(date +%s)
+    dir=$(dirname "$path")
+    echo "$path,$gval,$date,$timestamp,$dir" >>$MOLE_RC
+    "$editor_path" "$path"
+}
+
+Directory() {
     
+    awkin="cat $MOLE_RC | awk -F ',' '\$3 > \"$aval\" && \$3 < \"$bval\" && \$2 ~ /$gval/ {print \$0}' | sort -k4 -t ',' -n | tail -1"
+    idk=$(eval $awkin)
+    echo $idk
+    Filters
 }
 Exec() {
-    IsDirectory
-
+    IsFile
     if [ -n "$is_file" ]; then
         File
+    else
+        Directory
     fi
 }
-
-gflag=0
-aflag=0
-mflag=0
-bflag=0
 
 while getopts hg:mb:a: name; do
     case $name in
@@ -81,9 +117,11 @@ while getopts hg:mb:a: name; do
         ;;
     a)
         aflag+=1
+        aval="$OPTARG"
         ;;
     b)
         bflag+=1
+        bval="$OPTARG"
         ;;
     ?)
         printf "Usage: %s: [-a] [-b value] args\n" $0
@@ -92,13 +130,10 @@ while getopts hg:mb:a: name; do
     esac
 done
 
+CreateMoleRC
 shift "$((OPTIND - 1))"
-path=$1
+path=$(readlink -f $1)
 ArgError
 SetEditor
-echo "DEBUG: Using: $edit_path"
+echo "DEBUG: Using: $editor_path"
 Exec
-#create mole_rc
-if [[ ! -e $MOLE_RC ]]; then
-    touch $MOLE_RC
-fi
