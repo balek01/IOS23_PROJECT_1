b829b77b76014cc26baf5fba7b494c9338b013e9
Bálek Miroslav <mirbalek@seznam.cz>
2023-03-13 19:04:55 +0100

    list working

--- mole.sh
+++ mole.sh
@@ -9,6 +9,7 @@ aval=0000-00-00
 bval=9999-99-99
 gvalin=''
 dir=$(pwd)
+tail=1
 
 ArgError() {
     if [ "$gflag" -gt 1 ]; then
@@ -66,31 +67,33 @@ SetEditor() {
     elif [ -n "$VISUAL" ]; then
         editor_path="$VISUAL"
     else
-
         editor_path=$(which vi)
     fi
 }
 
 IsFile() {
-    if [ -d "$path" ]; then
-        echo "DEBUG: Directory is $path"
-    elif [ -f "$path" ]; then
+    echo "Given path $givenpath"
+    if [ -d "$givenpath" ]; then
+        echo "DEBUG: Directory is $givenpath"
+    elif [ -f "$givenpath" ]; then
         is_file=1
-        echo "DEBUG: File is $path"
+        echo "DEBUG: File is $givenpath"
     else
-        lastchar=${path: -1}
+        lastchar=${givenpath: -1}
         if [ "$lastchar" == "/" ]; then
-            echo "ERROR: NonExisting Directory is $path"
+            echo "ERROR: NonExisting Directory is $givenpath"
             exit 1
         elif [ -n "$lastchar" ]; then
             is_file=1
-            echo "DEBUG: NonExisting File is $path"
+            echo "DEBUG: NonExisting File is $givenpath"
         fi
     fi
 }
 
 RunEditor() {
-    if [ -n "$path" ]; then
+    if [ -n "$is_file" ]; then
+        "$editor_path" "$givenpath"
+    elif [ -n "$path" ]; then
         "$editor_path" "$path"
     else
         echo "Nothing matches given parameters"
@@ -112,42 +115,46 @@ UpdateRow() {
         IFS=','
         echo "${rowarr[*]}"
     )
-    echo "$row"
+    echo "$row" >>"$MOLE_RC"
 }
 
 GetAwkRow() {
     AWKROWBASE="cat $MOLE_RC | awk -F ',' '\$3 > \"$aval\" && \$3 < \"$bval\" && \$2~g  && \$5~p {print \$0}' g=\"$gval\"  p=\"^$path$\""
 
-    if [ "$mflag" -eq 0 ]; then
-        #noMflag
-        AWKROWTAIL=" | sort -k4 -t ',' -n | tail -1"
-    else
-        AWKROWTAIL=" | sort | uniq -c | sort -rn | head -n 1 | sed -E 's/^ *[0-9]+ //g'"
+    if [ ! "$is_list" ]; then
+        if [ "$mflag" -eq 0 ]; then
+            #noMflag
+            AWKROWTAIL=" | sort -k4 -t ',' -n | tail -$tail| head -n 1"
+        else
+            AWKROWTAIL=" | sort | uniq -c | sort -rn | head -n 1 | sed -E 's/^ *[0-9]+ //g'"
+        fi
     fi
-
     awkgetrow=$AWKROWBASE$AWKROWTAIL
     echo "$awkgetrow"
 
     row=$(eval "$awkgetrow")
+
     if [ -z "$row" ]; then
         echo "Nothing matches given parameters"
         exit 1
     fi
+
 }
 
 GetAwkPath() {
     awkpath="awk -F ',' '{print \$1}'<<< $row"
     path=$(eval "$awkpath")
+    echo "$path"
 }
 
 SetPath() {
-    if [ -z "$path" ]; then
+    if [ -z "$givenpath" ]; then
         path=$(pwd)
         path=$(realpath "$path")
         echo "DEBUG: DIR: $path"
     else
 
-        path=$(realpath "$path")
+        path=$(realpath "$givenpath")
 
     fi
 }
@@ -155,7 +162,12 @@ SetPath() {
 GetGroups() {
 
     IFS=',' read -ra groups <<<"$gval"
-    gval=""
+    if [ -z "$gval" ]; then
+        gval="^$|"
+    else
+        gval=""
+    fi
+
     # Loop over the array and print each element
     for group in "${groups[@]}"; do
 
@@ -164,53 +176,111 @@ GetGroups() {
     #always false
     gval+="/(?=a)b/"
 }
+GetExistingFile() {
 
-Directory() {
-    echo "DEBUG: In directory"
+    mole_rc_count=$(wc -l "$MOLE_RC" | awk '{print $1}')
 
+    if [ "$tail" -eq "$mole_rc_count" ]; then
+        echo "Nothing matches "
+        exit 1
+    fi
+
+    if [ ! -f "$path" ]; then
+        echo "File $path does not exist"
+        tail=$((tail + 1))
+        GetFile
+    fi
+
+}
+GetFile() {
     SetPath
-    GetGroups
     GetAwkRow
     GetAwkPath
-    UpdateRow
+    GetExistingFile
+}
+Directory() {
+    echo "DEBUG: In directory"
+    GetGroups
+    GetFile
 
+    UpdateRow
     RunEditor
 }
 
 CheckForCommas() {
 
     if [[ "$gval" == *","* ]]; then
-        echo "It's there."
+        echo "Commas are not allowed in group name."
         exit 1
     fi
 }
 
 AddRow() {
-    dir=$(realpath "$path")
-    dir=$(dirname "$dir")
-    echo "$path,$gvalin,$date,$timestamp,$dir" >>"$MOLE_RC"
-}
-
-OpenEditor() {
-    "$editor_path" "$path"
+    realpath=$(realpath "$givenpath")
+    file=$(basename "$realpath")
+    dir=$(dirname "$realpath")
+    echo "$givenpath,$gvalin,$date,$timestamp,$dir,$file" >>"$MOLE_RC"
 }
 
 File() {
     GetTimestampDate
     CheckForCommas
-    OpenEditor
+    AddRow
+    RunEditor
+}
+GetUnsortedList() {
+    unsorted=$(echo "$row" | cut -d ',' -f 2,6 | sort | uniq | awk -F ',' '{
+  paths[$2][length(paths[$2])] = $1
+} END {
+  for (path in paths) {
+    count = 0
+    printf("%s: ;", path)
+    comma = ""
 
+    for (i = 0; i < length(paths[path]); i++) {
+      if (paths[path][i] != "") {
+        count++
+        printf("%s%s", comma, paths[path][i])
+        comma = ","
+      }
+    }
+
+    if (count == 0) {
+      printf("-")
+    }
+
+    print ""
+  }
+}' | column -t -s ";" | sed 's/ //1' | sort)
+    echo "$unsorted"
 }
 
+List() {
+    SetPath
+    GetGroups
+    GetAwkRow
+    GetUnsortedList
+
+}
 Exec() {
-    IsFile
-    if [ -n "$is_file" ]; then
-        File
+    if [ -n "$is_list" ]; then
+
+        List
     else
-        Directory
+        IsFile
+        if [ -n "$is_file" ]; then
+            File
+        else
+            Directory
+        fi
     fi
 }
 
+if [ "$1" = "list" ]; then
+    is_list=1
+    shift
+
+fi
 while getopts dhg:mb:a: name; do
     case $name in
     h) PrintHelp ;;
@@ -232,7 +302,7 @@ while getopts dhg:mb:a: name; do
         ;;
     d)
         dflag+=1
-        gval=''
+        gval='.*'
         ;;
     ?)
         printf "Usage: %s: [-a] [-b value] args\n" "$0"
@@ -242,17 +312,17 @@ while getopts dhg:mb:a: name; do
 done
 
 CreateMoleRC
+
 shift "$((OPTIND - 1))"
 
 if [ -n "$1" ]; then
-    path=$(readlink -f "$1")
+    givenpath=$(readlink -f "$1")
 
 fi
 
-if [ -z "$gval" ]; then
-    gval='.*'
-fi
 ArgError
 SetEditor
 echo "DEBUG: Using: $editor_path"
 Exec
+
+exit 0
